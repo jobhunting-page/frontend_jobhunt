03bd583a327af2b173dbdc038cc43c799ac8b2da
jieun <jieunc023@gmail.com>
2023-05-09 20:38:41 +0900

    feat: 게시판 조회 및 글 작성, 이메일 인증, gpt 수정

--- src/js/Auth/Join.jsx
+++ src/js/Auth/Join.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import logo from '../../images/logo.png';
 import { useNavigate } from "react-router-dom";
+import qs from 'qs';
 
 function Join() {
   const [email, setInputEmail] = useState("");
@@ -19,6 +20,13 @@ function Join() {
   let [id_, setId] = useState();
   let [pass_, setPass] = useState();
   let [nickname_, setNickName] = useState();
+  const [emailCheck, setEmailCheck] = useState();
+  const [code, setCode] = useState();
+  const [codeInput, setCodeInput] = useState('');
+  const [isConfirm, setConfirm] = useState(0);
+  const [isActive, setIsActive] = useState(0);
+  const [emailWaiting, setEmailWaiting] = useState(true);
+
 
   const navigate = useNavigate();
 
@@ -57,6 +65,15 @@ function Join() {
   const handleInputPhone = (e) => {
     setInputPhone(e.target.value);
   };
+
+  const handeInputEmailCheck = (e) => {
+    setEmailCheck(e.target.value);
+  }
+
+  const handeInputCode = (e) => {
+    setCodeInput(e.target.value);
+  }
+
 
   const idCheck = () => {
     // axios
@@ -115,6 +132,44 @@ function Join() {
     //   .catch();
   };
 
+  const onClickEmailCheck = () => {
+    const jsonData = JSON.stringify(emailCheck);
+    axios({
+      method: 'post',
+      url: '/api/mail',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: jsonData
+    }).then((res) => {
+      setEmailWaiting(false);
+      const data = res.data;
+      console.log(data);
+      setCode(data);
+      setConfirm(1);
+      console.log(emailWaiting);
+    }).catch((err) => {
+      console.log(err);
+    })
+    if (emailWaiting) {
+      setTimeout( () => {
+        setEmailWaiting(true);
+      }, 30000)
+    }
+  }
+  const onClickCodeCheck = () => {
+    
+    if (code == codeInput) {
+      setIsActive(1);
+    }
+    else {
+      Swal.fire({
+        icon: 'warning',
+        title: '인증번호가 올바르지 않습니다!',
+    })
+    }
+  }
+
   const onClickJoin = () => {
     // alert(email, pass, pass2, nickname, sex, birth, phone)
         console.log("click Join");
@@ -169,7 +224,15 @@ function Join() {
 	  	<input type="password" path="pass2" placeholder="비밀번호 확인" id="pass2" name="pass2" value={pass2} onChange={handleInputPw2} onKeyUp={pwCheck} />
       <div style={ pass_ === 0 ? {display:"block"} : {display:"none"}}>비밀번호가 일치하지 않습니다</div>
 	  </div>
-	  <div className="LoginBtn" onClick={onClickJoin}>가입하기</div>
+    <div className="InputEmail">
+	  	<input type="text" path="emailCheck" placeholder="이메일 인증" id="emailCheck" name="emailCheck" value={emailCheck} onChange={handeInputEmailCheck} required/>
+      <button className="EmailBtn" onClick={onClickEmailCheck} disabled={!(emailCheck  && emailWaiting)}>이메일 전송</button>
+	  </div>
+    <div className="InputEmail">
+	  	<input type="text" path="codeInput" placeholder="인증번호" id="codeInput" name="codeInput" value={codeInput} onChange={handeInputCode} required/>
+      <button className="EmailBtn" onClick={onClickCodeCheck} disabled={!(isConfirm)}>인증하기</button>
+	  </div>
+    <button className="LoginBtn" onClick={onClickJoin} disabled={!isActive}>가입하기</button>
 	</div>
   )
 }
